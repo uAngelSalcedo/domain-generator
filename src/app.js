a62b199c7ee66abad69218e63a02aedbd5f490a4
uAngelSalcedo <162602493+uAngelSalcedo@users.noreply.github.com>
2024-06-05 00:14:38 +0000

    domain-generator

--- src/app.js
+++ src/app.js
@@ -1,11 +1,22 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  const dominio = document.getElementById("dominio");
+  function domainGenerator() {
+    let pronoun = ["the", "our"];
+    let adj = ["great", "big"];
+    let noun = ["jogger", "racoon"];
+    let ext = [".com", ".es"];
+
+    for (let a = 0; a < pronoun.length; a++) {
+      for (let b = 0; b < adj.length; b++) {
+        for (let c = 0; c < noun.length; c++) {
+          for (let d = 0; d < ext.length; d++) {
+            let resultado = `${pronoun[a]}${adj[b]}${noun[c]}${ext[d]}`;
+            dominio.innerHTML += `<p>${resultado}<p/>`;
+            console.log(resultado);
+          }
+        }
+      }
+    }
+  }
+  domainGenerator();
 };
